347e9a525c9200ec8f1d98e8cab3779482b2f982
NivedaR <nivedarangarajan@gmail.com>
2017-11-18 15:58:14 +0000

    conflict resolving

--- bms.cpp
+++ bms.cpp
@@ -5,8 +5,15 @@ BatteryManagementSystem::BatteryManagementSystem()
 }
 
 /// TASK 9 (BOTH) //////////////////////////////////////////////////////////////
+
 double BatteryManagementSystem::get_current()
 {
   return 30;
 }
+
+double BatteryManagementSystem::get_voltage()
+{
+  return 0;
+}
+
 /// END OF TASKS 9AB ///////////////////////////////////////////////////////////
